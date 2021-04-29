024907950b5bc8b454479e9eb35cfefe5df6caef
Aarav Varshney <aaravvarshney22@gmail.com>
2021-04-30 03:22:04 +0530

    cards modified

--- src/screens/home/ModuleInfo.js
+++ src/screens/home/ModuleInfo.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { View, Image, SafeAreaView, Dimensions, Text } from "react-native";
+import { Separators, Buttons, Lists, Header } from "_components";
 import Carousel from 'react-native-snap-carousel';
+
 import styles from "./ModuleStyles";
 
 const windowWidth = Dimensions.get('window').width;
@@ -17,12 +19,26 @@ const ModuleInfo = ({ navigation }) => {
     const renderSlides = ({ item }) => {
         return (
             <View style={styles.itemContainer}>
-                <Text style={styles.itemLabel}>{`Item ${item}`}</Text>
+                <View style={styles.carouselHeading}>
+                    <Text style={styles.carouselHeadingText}>Section Heading</Text>
+                </View>
+                <View style={styles.carouselTextContainer}>
+                <Text style={styles.carouselText}>
+                All the information will be in these cards which can be swiped right and left to move forward and back. They will be broken down in snippets as thats how people retain the most. mixture of videos, pictures and text. preferably one picture and some text like like card because people remember pictures more easily and by relating it to the accompanying text, retention will be even more. 
+                </Text>
+                </View>
             </View>
         );
     }
     return (
         <SafeAreaView style={styles.mainContainer}>
+            <Header.Default />
+            <View style={styles.headingContainer}>
+                <Text style={styles.headingText}>Module Name</Text>
+            </View>
+            <View style={styles.descriptionContainer}>
+                <Text style={styles.descriptionText}>A short description of what the topics that this module covers. cannto be longer than this much. </Text>
+            </View>
             <Carousel
                 data={DATA}
                 layout={"default"}
@@ -30,7 +46,7 @@ const ModuleInfo = ({ navigation }) => {
                 sliderWidth={windowWidth}
                 itemWidth={300}
                 onSnapToItem={(index) => setIndex(index)}
-                useScrollView={true}
+                useScrollView={false}
             />
             <Text style={styles.counter}
             >
--- src/screens/home/ModuleStyles.js
+++ src/screens/home/ModuleStyles.js
@@ -10,19 +10,58 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#EDF1F9",
     },
+    headingContainer: {
+        alignSelf:'center'
+    }, 
+    headingText: {
+        fontFamily: "TitilliumWeb-SemiBold",
+        color: "#0D1F3C",
+        fontSize: 26,
+    }, 
+    descriptionContainer: {
+        top: 10,
+        width: windowHeight*0.4,
+        alignSelf: 'center'
+    },
+    descriptionText: {
+        textAlign: 'center',
+        fontFamily: "TitilliumWeb-Regular",
+        color: "#485068",
+        fontSize: 15,
+    },
+    carouselHeading: {
+        alignItems: 'center',
+        top:20,
+    },
+    carouselHeadingText: {
+        fontFamily: "TitilliumWeb-SemiBold",
+        color: "#0D1F3C",
+        fontSize: 26,
+    },
+
+    carouselTextContainer: {
+        top: 150,
+        alignSelf:'center',
+        width: windowWidth * 0.7
+    },
+    carouselText: {
+        textAlign: 'center',
+        fontFamily: "TitilliumWeb-Regular",
+        color: "#0D1F3C",
+        fontSize: 15,
+    },
+
     carouselContainer: {
         marginTop: 100
     },
     itemContainer: {
-        top: 150,
+        top: 80,
         width: windowWidth * 0.8,
         height: windowHeight * 0.6,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'dodgerblue'
+        backgroundColor: '#FFFFFF'
     },
     itemLabel: {
-        color: 'white',
+        color: 'blue',
         fontSize: 24
     },
     counter: {
