303077bcaec0c38bf7d6fce20eb2bbbe0db81d7b
Aarav Varshney <aaravvarshney22@gmail.com>
2021-04-30 03:46:51 +0530

    tab nav 0.5

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import Home from '../home'
 import Transactions from '../transactions'
 
@@ -8,8 +9,20 @@ const Tab = createBottomTabNavigator();
 
 const Main = () => {
     return (
-        <Tab.Navigator initialRouteName="Transactions">
-            <Tab.Screen name="Transactions" component={Transactions} />
+        <Tab.Navigator initialRouteName="Transactions"
+            tabBarOptions={{
+                style: {
+                    backgroundColor: "#EDF1F9",
+                    borderRadius: 100,
+                    elevation: 3,
+                }
+            }}>
+            <Tab.Screen name="Transactions" component={Transactions} 
+            options={{
+                tabBarIcon: () => {
+                    <Icon name="money-check" size={30} color={"red"}/>
+                }
+            }}/>
             <Tab.Screen name="Learning" component={Home} />
         </Tab.Navigator>
     );
